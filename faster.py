e2c6039cd17967aba73a989d9e87be11aacb2d9e
Ryan Uber <ru@ryanuber.com>
2014-01-07 10:07:02 -0800

    Initial commit - Currently just a straight monkeypatch of YumBase.downloadPkgs - now to refine the method itself.

---
+++ faster.py
@@ -0,0 +1,161 @@
+from yum.plugins import TYPE_CORE
+from yum import YumBase
+import yum.packages
+import os
+import time
+import yum.i18n
+_ = yum.i18n._
+P_ = yum.i18n.P_
+import urlgrabber
+from urlgrabber.grabber import URLGrabber, URLGrabError
+from urlgrabber.progress import format_number
+
+requires_api_version = '2.5'
+plugin_type = (TYPE_CORE,)
+
+class _yb(YumBase):
+    def downloadPkgs(self, pkglist, callback=None, callback_total=None):
+        print "LKSJDFLKSJF"
+        def mediasort(apo, bpo):
+            # FIXME: we should probably also use the mediaid; else we
+            # could conceivably ping-pong between different disc1's
+            a = apo.getDiscNum()
+            b = bpo.getDiscNum()
+            if a is None and b is None:
+                return cmp(apo, bpo)
+            if a is None:
+                return -1
+            if b is None:
+                return 1
+            if a < b:
+                return -1
+            elif a > b:
+                return 1
+            return 0
+
+        """download list of package objects handed to you, output based on
+           callback, raise yum.Errors.YumBaseError on problems"""
+
+        errors = {}
+        def adderror(po, msg):
+            errors.setdefault(po, []).append(msg)
+
+        #  We close the history DB here because some plugins (presto) use
+        # threads. And sqlite really doesn't like threads. And while I don't
+        # think it should matter, we've had some reports of history DB
+        # corruption, and it was implied that it happened just after C-c
+        # at download time and this is a safe thing to do.
+        #  Note that manual testing shows that history is not connected by
+        # this point, from the cli with no plugins. So this really does
+        # nothing *sigh*.
+        self.history.close()
+
+        self.plugins.run('predownload', pkglist=pkglist)
+        repo_cached = False
+        remote_pkgs = []
+        remote_size = 0
+        for po in pkglist:
+            if hasattr(po, 'pkgtype') and po.pkgtype == 'local':
+                continue
+            local = po.localPkg()
+            if os.path.exists(local):
+                if not self.verifyPkg(local, po, False):
+                    if po.repo.cache:
+                        repo_cached = True
+                        adderror(po, _('package fails checksum but caching is '
+                            'enabled for %s') % po.repo.id)
+                else:
+                    self.verbose_logger.debug(_("using local copy of %s") %(po,))
+                    continue
+            remote_pkgs.append(po)
+            remote_size += po.size
+            # caching is enabled and the package
+            # just failed to check out there's no
+            # way to save this, report the error and return
+            if (self.conf.cache or repo_cached) and errors:
+                return errors
+
+        remote_pkgs.sort(mediasort)
+        #  This is kind of a hack and does nothing in non-Fedora versions,
+        # we'll fix it one way or anther soon.
+        if (hasattr(urlgrabber.progress, 'text_meter_total_size') and
+            len(remote_pkgs) > 1):
+            urlgrabber.progress.text_meter_total_size(remote_size)
+        beg_download = time.time()
+        i = 0
+        local_size = 0
+        done_repos = set()
+        for po in remote_pkgs:
+            #  Recheck if the file is there, works around a couple of weird
+            # edge cases.
+            local = po.localPkg()
+            i += 1
+            if os.path.exists(local):
+                if self.verifyPkg(local, po, False):
+                    self.verbose_logger.debug(_("using local copy of %s") %(po,))
+                    remote_size -= po.size
+                    if hasattr(urlgrabber.progress, 'text_meter_total_size'):
+                        urlgrabber.progress.text_meter_total_size(remote_size,
+                                                                  local_size)
+                    continue
+                if os.path.getsize(local) >= po.size:
+                    os.unlink(local)
+
+            checkfunc = (self.verifyPkg, (po, 1), {})
+            dirstat = os.statvfs(po.repo.pkgdir)
+            if (dirstat.f_bavail * dirstat.f_bsize) <= long(po.size):
+                adderror(po, _('Insufficient space in download directory %s\n'
+                        "    * free   %s\n"
+                        "    * needed %s") %
+                         (po.repo.pkgdir,
+                          format_number(dirstat.f_bavail * dirstat.f_bsize),
+                          format_number(po.size)))
+                continue
+            try:
+                if i == 1 and not local_size and remote_size == po.size:
+                    text = os.path.basename(po.relativepath)
+                else:
+                    text = '(%s/%s): %s' % (i, len(remote_pkgs),
+                                            os.path.basename(po.relativepath))
+                mylocal = po.repo.getPackage(po,
+                                   checkfunc=checkfunc,
+                                   text=text,
+                                   cache=po.repo.http_caching != 'none',
+                                   )
+                local_size += po.size
+                if hasattr(urlgrabber.progress, 'text_meter_total_size'):
+                    urlgrabber.progress.text_meter_total_size(remote_size,
+                                                              local_size)
+                # Don't check this atm. as it kills C-c.
+                if False and po.repoid not in done_repos:
+                    #  Check a single package per. repo. ... to give a hint to
+                    # the user on big downloads.
+                    result, errmsg = self.sigCheckPkg(po)
+                    if result != 0:
+                        self.verbose_logger.warn("%s", errmsg)
+                done_repos.add(po.repoid)
+
+            except Errors.RepoError, e:
+                adderror(po, exception2msg(e))
+            else:
+                po.localpath = mylocal
+                if po in errors:
+                    del errors[po]
+
+        if hasattr(urlgrabber.progress, 'text_meter_total_size'):
+            urlgrabber.progress.text_meter_total_size(0)
+        if callback_total is not None and not errors:
+            callback_total(remote_pkgs, remote_size, beg_download)
+
+        self.plugins.run('postdownload', pkglist=pkglist, errors=errors)
+
+        # Close curl object after we've downloaded everything.
+        if hasattr(urlgrabber.grabber, 'reset_curl_obj'):
+            urlgrabber.grabber.reset_curl_obj()
+
+        return errors
+
+def init_hook(conduit):
+    if hasattr(conduit, 'registerPackageName'):
+        conduit.registerPackageName('yum-faster')
+    conduit._base.downloadPkgs = _yb().downloadPkgs
